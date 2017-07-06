1ebcf97b8bde37628fc0a0397cd4abf13db98a2c
cstapiarubio <cstapiarubio@gmail.com>
2017-07-06 13:01:35 -0400

    primer commit

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Giphy</title>
+	<!-- fonts google-->
+	<link href="https://fonts.googleapis.com/css?family=Artifika|Open+Sans" rel="stylesheet">
+	<!--iconos (fonts awesome)-->
+	<script src="https://use.fontawesome.com/2c0c95ddaa.js"></script>
+	<!-- CDN bootstrap css-->
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- CSS-->
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+	
+</head>
+
+<body>
+
+</body>
+
+</html>
+
